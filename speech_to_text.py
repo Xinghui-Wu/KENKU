6b5a879117e9f0b72ab507eb0ad9fdc0f8d73307
wuxinghui <xjtu.wuxinghui@gmail.com>
2020-11-15 16:12:50 +0800

    Integrate the API of the ASR service of iFLYTEK into speech_to_text.py

--- speech_to_text.py
+++ speech_to_text.py
@@ -4,12 +4,15 @@ import csv
 
 from aip import AipSpeech
 
+from iflytek import RequestApi
 
-APP_ID = "17156719"
-API_KEY = "aqZ67xX12E6umtTXfw44kYi4"
-SECRET_KEY = "w2fLg5VzQMcumFHgtPpvsXsPAj65FUyw"
 
-CLIENT = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
+BAIDU = AipSpeech(appId="17156719", 
+                  apiKey="aqZ67xX12E6umtTXfw44kYi4", 
+                  secretKey="w2fLg5VzQMcumFHgtPpvsXsPAj65FUyw")
+
+IFLYTEK_APPID = "5fb0c096"
+IFLYTEK_SECRET_KEY = "4e5779d189ece9a31a0d526054513cc2"
 
 
 def speech_to_text(directory, transcriptions, output):
@@ -25,8 +28,8 @@ def speech_to_text(directory, transcriptions, output):
     audio_filenames = os.listdir(directory)
     audio_filenames.sort()
 
-    with open(transcriptions, 'r') as transcription_txt:
-        transcriptions = transcription_txt.readlines()
+    with open(transcriptions, 'r') as transcriptions_txt:
+        transcriptions = transcriptions_txt.readlines()
     
     for i in range(len(audio_filenames)):
         audio_path = os.path.join(directory, audio_filenames[i])
@@ -53,7 +56,7 @@ def baidu_speech_to_text(audio_path):
     with open(audio_path, 'rb') as audio_file:
         audio = audio_file.read()
     
-    asr_result = CLIENT.asr(audio, 'wav', 16000, {'dev_pid': 1537})
+    asr_result = BAIDU.asr(audio, 'wav', 16000, {'dev_pid': 1537})
 
     if 'result' in asr_result:
         return asr_result['result'][0]
@@ -70,14 +73,24 @@ def iflytek_speech_to_text(audio_path):
     Returns:
         [type]: [description]
     """
+    iFlytek_asr = RequestApi(appid=IFLYTEK_APPID, secret_key=IFLYTEK_SECRET_KEY, upload_file_path=audio_path)
+
+    asr_result = iFlytek_asr.all_api_request()
+
+    if 'data' in asr_result:
+        asr_result = asr_result['data']
+
+        if asr_result != None:
+            return eval(asr_result)[0]['onebest']
+
     return ""
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("-d", "--directory", type=str, default="./Audio Samples/Malicious Samples/", help="")
-    parser.add_argument("-t", "--transcriptions", type=str, default="./Audio Samples/Commands.txt", help="")
-    parser.add_argument("-o", "--output", type=str, default="./Audio Samples/ASR_Results.csv", help="")
+    parser.add_argument("-t", "--transcriptions", type=str, default="./Audio Samples/Malicious-Commands.txt", help="")
+    parser.add_argument("-o", "--output", type=str, default="./Audio Samples/ASR-Results.csv", help="")
     args = parser.parse_args()
 
     speech_to_text(directory=args.directory, transcriptions=args.transcriptions, output=args.output)
