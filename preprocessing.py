33a3cd77d270a7e32537f8f6fef11399754be850
wuxinghui <xjtu.wuxinghui@gmail.com>
2020-11-20 22:16:33 +0800

    Add comments.

--- preprocessing.py
+++ preprocessing.py
@@ -5,10 +5,11 @@ from pydub import AudioSegment
 
 
 def preprocess_songs(songs):
-    """[summary]
+    """Preprocess the song files to make them the specified format.
+    The songs used in the attack method must be a mono wav file with a sampling rate of 16kHz.
 
     Args:
-        songs ([type]): [description]
+        songs (str): Input directory of some song files.
     """
     song_filenames = os.listdir(songs)
 
@@ -20,8 +21,8 @@ def preprocess_songs(songs):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="")
-    parser.add_argument("-s", "--songs", type=str, default="./Audio Samples/Songs/", help="")
+    parser = argparse.ArgumentParser(description="Preprocess the song files to make them the specified format.")
+    parser.add_argument("-s", "--songs", type=str, default="./Audio Samples/Songs/", help="Input directory of some song files.")
     args = parser.parse_args()
 
     preprocess_songs(songs=args.songs)
