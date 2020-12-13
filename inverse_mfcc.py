9df2982ab0d5ef977f59dae7e2652913c550cde1
wuxinghui <xjtu.wuxinghui@gmail.com>
2020-12-14 01:22:10 +0800

    Commit inverse_mfcc.py based on Griffin-Lim.

---
+++ inverse_mfcc.py
@@ -0,0 +1,65 @@
+import argparse
+import os
+
+import numpy as np
+from librosa import load
+from librosa.feature import mfcc
+from librosa.feature.inverse import mfcc_to_audio
+from soundfile import write
+
+
+def attack(commands, malicious_samples, transcriptions, num_iterations):
+    """An attack method based on Griffin-Lim to reverse MFCC
+    For each command, get its MFCC and try to reverse it back to a time-domain signal for multiple times.
+
+    Args:
+        commands (str): Input directory of some command files.
+        malicious_samples (str): Output directory of the generated malicious samples.
+        transcriptions (str): Path of the transcription file of the commands.
+        num_iterations (int): The number of iterations of inverse MFCC.
+    """
+    command_filenames = os.listdir(commands)
+    command_filenames.sort(key=lambda filename: os.path.getmtime(os.path.join(commands, filename)))
+
+    with open(transcriptions, 'r') as transcription_txt:
+        command_transcriptions = transcription_txt.readlines()
+    
+    malicious_transcriptions = list()
+
+    for i in range(len(command_filenames)):
+        print("****************************************************************************************************")
+        
+        command_path = os.path.join(commands, command_filenames[i])
+        command, _ = load(path=command_path, sr=16000)
+        target_command_mfcc = mfcc(y=command, sr=16000)
+
+        for j in range(num_iterations):
+            print(command_filenames[i], j)
+
+            command_mfcc = mfcc(y=command, sr=16000)
+            command = mfcc_to_audio(mfcc=command_mfcc)
+
+            print("||malicious_sample_mfcc - command_mfcc|| = {:.4f}".format(np.linalg.norm(mfcc(y=command, sr=16000) - target_command_mfcc)))
+
+            malicious_sample_path = os.path.join(malicious_samples, "Malicious-Sample-{}-{}.wav".format(i, j))
+            write(file=malicious_sample_path, data=command, samplerate=16000)
+
+            malicious_transcriptions.append(command_transcriptions[i])
+        
+        print("****************************************************************************************************")
+        print()
+    
+    with open(os.path.join(os.path.dirname(transcriptions), "Malicious-Commands.txt"), 'w') as malicious_transcriptions_txt:
+        malicious_transcriptions_txt.writelines(malicious_transcriptions)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="An attack method based on Griffin-Lim to reverse MFCC")
+    parser.add_argument("-c", "--commands", type=str, default="./Audio Samples/Commands/", help="Input directory of some command files.")
+    parser.add_argument("-m", "--malicious_samples", type=str, default="./Audio Samples/Malicious Samples/", help="Output directory of the generated malicious samples.")
+    parser.add_argument("-t", "--transcriptions", type=str, default="./Audio Samples/Commands.txt", help="Path of the transcription file of the commands.")
+    parser.add_argument("-n", "--num_iterations", type=int, default=5, help="The number of iterations of inverse MFCC.")
+    args = parser.parse_args()
+    
+    attack(commands=args.commands, malicious_samples=args.malicious_samples, 
+           transcriptions=args.transcriptions, num_iterations=args.num_iterations)
