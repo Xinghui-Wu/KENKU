d55cc38e7262edf9203b3d1bede93ef496a024aa
wuxinghui <xjtu.wuxinghui@gmail.com>
2021-04-29 22:30:53 +0800

    Remove the CMU Sphinx installation script.

--- CMU-Sphinx-zh-CN-install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-SR_LIB=$(python -c "import speech_recognition as sr, os.path as p; print(p.dirname(sr.__file__))")
-
-sudo unzip -o zh-CN.zip -d "$SR_LIB"
-sudo chmod --recursive a+r "$SR_LIB/zh-CN/"
