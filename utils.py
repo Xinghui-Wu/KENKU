714ab96382c098da367e8deb63d8ee4c2012b17e
wuxinghui <xjtu.wuxinghui@gmail.com>
2021-11-23 20:34:43 +0800

    Move the get_feature_parameters() function to utils.py

--- utils.py
+++ utils.py
@@ -13,3 +13,26 @@ def write_csv(csv_path, lines):
     with open(csv_path, 'w') as csv_file:
         csv_writer = csv.writer(csv_file)
         csv_writer.writerows(lines)
+
+
+def get_feature_parameters(feature):
+    feature_parameters = list()
+
+    n_mfcc_list = (12, 13, 20, 26, 40)
+    n_mels_list = (64, 128, 256, 512)
+    n_fft_list = (512, 1024, 2048)
+
+    if feature == 1:
+        for n_fft in n_fft_list:
+            feature_parameters.append({"n_fft": n_fft})
+    elif feature == 2:
+        for n_mels in n_mels_list:
+            for n_fft in n_fft_list:
+                feature_parameters.append({"n_mels": n_mels, "n_fft": n_fft})
+    else:
+        for n_mfcc in n_mfcc_list:
+            for n_mels in n_mels_list:
+                for n_fft in n_fft_list:
+                    feature_parameters.append({"n_mfcc": n_mfcc, "n_mels": n_mels, "n_fft": n_fft})
+    
+    return feature_parameters
