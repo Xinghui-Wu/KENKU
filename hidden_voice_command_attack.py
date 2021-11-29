5b95d423a8997dd1e31801743474a06f9aed6c8c
wuxinghui <xjtu.wuxinghui@gmail.com>
2021-11-29 20:58:57 +0800

    Record the trend of loss and SNR.

--- hidden_voice_command_attack.py
+++ hidden_voice_command_attack.py
@@ -4,17 +4,18 @@ import logging
 import traceback
 
 import numpy as np
+import matplotlib.pyplot as plt
 from librosa import load, stft, griffinlim
 from librosa.feature import mfcc, melspectrogram
 from librosa.feature.inverse import mfcc_to_audio, mel_to_audio
 from soundfile import write
 
-from utils import read_csv, write_csv, get_feature_parameters
+from utils import *
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s \t %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 
 
-def hidden_voice_command_attack(feature, command_csv, dir, num_iterations):
+def hidden_voice_command_attack(feature, command_csv, dir, num_iterations, interval):
     assert feature in (1, 2, 3)
 
     feature_parameters = get_feature_parameters(feature)
@@ -29,13 +30,16 @@ def hidden_voice_command_attack(feature, command_csv, dir, num_iterations):
 
         if not os.path.exists(hidden_voice_command_dir):
             os.makedirs(hidden_voice_command_dir)
-            
+        
         command, _ = load(path=command_info[0], sr=16000)
         
         for feature_parameters_dict in feature_parameters:
             command_feature = audio_to_feature(command, feature, feature_parameters_dict)
             hidden_voice_command = command
             hidden_voice_command_feature = command_feature
+
+            loss_trend = np.zeros(num_iterations)
+            snr_trend = np.zeros(num_iterations)
 
             try:
                 for i in range(num_iterations):
@@ -50,10 +54,22 @@ def hidden_voice_command_attack(feature, command_csv, dir, num_iterations):
                     hidden_voice_command = feature_to_audio(hidden_voice_command_feature, feature, feature_parameters_dict)
                     hidden_voice_command_feature = audio_to_feature(hidden_voice_command, feature, feature_parameters_dict)
 
-                    logging.info("loss = {:.4f}".format(np.linalg.norm(hidden_voice_command_feature - command_feature)))
+                    loss = np.linalg.norm(hidden_voice_command_feature - command_feature)
+                    snr = get_snr(audio=command, audio_with_noise=hidden_voice_command)
 
-                    hidden_voice_command_table.append([hidden_voice_command_path, command_info[1]])
-                    write(file=hidden_voice_command_path, data=hidden_voice_command, samplerate=16000)
+                    loss_trend[i] = loss
+                    snr_trend[i] = snr
+
+                    if (i + 1) % interval == 0:
+                        hidden_voice_command_table.append([hidden_voice_command_path, command_info[1], loss, snr])
+                        write(file=hidden_voice_command_path, data=hidden_voice_command, samplerate=16000)
+                    
+                    logging.info("loss = {:.4f}, snr = {:.2f}".format(loss, snr))
+                
+                fig, ax = plt.subplots(nrows=1, ncols=2, sharex='row')
+                ax[0].plot(loss_trend)
+                ax[1].plot(snr_trend)
+                fig.savefig("{}.png".format(hidden_voice_command_path))
             except:
                 logging.error("Error in {}".format(hidden_voice_command_path))
                 traceback.print_exc()
@@ -91,11 +107,13 @@ def feature_to_audio(audio_feature, feature, feature_parameters_dict):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument("-f", "--feature", type=int, default=3, help="")
+    parser.add_argument("-f", "--feature", type=int, default=1, help="")
     parser.add_argument("-c", "--command_csv", type=str, default="commands/commands.csv", help="")
     parser.add_argument("-d", "--dir", type=str, default="hidden-voice-commands/", help="")
-    parser.add_argument("-n", "--num_iterations", type=int, default=10, help="")
+    parser.add_argument("-n", "--num_iterations", type=int, default=10000, help="")
+    parser.add_argument("-i", "--interval", type=int, default=100, help="")
 
     args = parser.parse_args()
 
-    hidden_voice_command_attack(feature=args.feature, command_csv=args.command_csv, dir=args.dir, num_iterations=args.num_iterations)
+    hidden_voice_command_attack(feature=args.feature, command_csv=args.command_csv, dir=args.dir, 
+                                num_iterations=args.num_iterations, interval=args.interval)
--- integrated_command_attack.py
+++ integrated_command_attack.py
@@ -2,13 +2,14 @@ import argparse
 import os
 import logging
 
+import matplotlib.pyplot as plt
 import torch
 from torch.autograd import Variable
 from torch.optim import Adam, SGD
 from torchaudio import load, save
 from torchaudio.transforms import MFCC, MelSpectrogram, Spectrogram
 
-from utils import read_csv, write_csv, get_feature_parameters
+from utils import *
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s \t %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 
@@ -46,26 +47,20 @@ def integrated_command_attack(feature, command_csv, song_dir, dir, interval, opt
 
                 for origin in range(0, song.size()[1] - command.size()[1] + 1, int(16000 * interval)):
                     time_origin = format(origin / 16000, '.1f')
-
-                    integrated_command_path = os.path.join(integrated_command_dir, 
-                                                           "{}-{}-{}-{}-{}-{}-{}-{}-{}-{}.wav".format(command_name, song_filename, time_origin, 
-                                                                                                      feature_parameters_dict.get("n_mfcc", ""), 
-                                                                                                      feature_parameters_dict.get("n_mels", ""), 
-                                                                                                      feature_parameters_dict.get("n_fft", ""), 
-                                                                                                      optimizer, penalty, learning_rate, num_iterations))
-                    song_clip_path = os.path.join(integrated_command_dir, 
-                                                  "{}-{}-{}-{}-{}-{}-{}-{}-{}-{}.wav".format(command_name, song_filename, time_origin, 
+                    integrated_command_filename = "{}-{}-{}-{}-{}-{}-{}-{}-{}-{}.wav".format(command_name, song_filename, time_origin, 
                                                                                              feature_parameters_dict.get("n_mfcc", ""), 
                                                                                              feature_parameters_dict.get("n_mels", ""), 
                                                                                              feature_parameters_dict.get("n_fft", ""), 
-                                                                                             optimizer, penalty, learning_rate, num_iterations))
+                                                                                             optimizer, penalty, learning_rate, num_iterations)
+                    integrated_command_path = os.path.join(integrated_command_dir, integrated_command_filename)
+                    song_clip_path = os.path.join(song_clip_dir, integrated_command_filename)
                     
                     logging.info("Start to generate {}".format(integrated_command_path))
 
-                    attack_sample(feature_extractor, command_info[0], song_path, integrated_command_path, song_clip_path, origin, 
-                                  penalty, optimizer, learning_rate, num_iterations)
+                    loss_feature, loss_delta, loss, snr = attack_sample(feature_extractor, command_info[0], song_path, integrated_command_path, song_clip_path, origin, 
+                                                                        penalty, optimizer, learning_rate, num_iterations)
 
-                    integrated_command_table.append([integrated_command_path, command_info[1]])
+                    integrated_command_table.append([integrated_command_path, command_info[1], loss_feature, loss_delta, loss, snr])
     
     write_csv(csv_path=os.path.join(dir, "integrated-commands-{}.csv".format(feature)), lines=integrated_command_table)
 
@@ -91,15 +86,16 @@ def attack_sample(feature_extractor, command_path, song_path, integrated_command
     command = command.to(DEVICE)
     song = song.to(DEVICE)
 
-    command_feature = feature_extractor(waveform=command)
-
     # Initialize the perturbation vector.
     delta = Variable(torch.zeros(size=song.size()).to(DEVICE), requires_grad=True)
+    command_feature = feature_extractor(waveform=command)
     integrated_command_feature = feature_extractor(waveform=song+delta)
 
-    logging.info("loss_feature = {:.4f}, loss_delta = {:.4f}, loss = {:.4f}".format(torch.norm(integrated_command_feature - command_feature).data, 
-                                                                                    torch.norm(delta).data, 
-                                                                                    torch.norm(integrated_command_feature - command_feature).data + penalty * torch.norm(delta).data))
+    loss_feature = torch.norm(integrated_command_feature - command_feature)
+    loss_delta = torch.norm(delta)
+    loss = loss_feature + penalty * loss_delta
+    
+    logging.info("loss_feature = {:.4f}, loss_delta = {:.4f}, loss = {:.4f}".format(loss_feature.data, loss_delta.data, loss.data))
 
     # Choose an optimizer. You can add more choices if needed.
     if optimizer == "Adam":
@@ -110,38 +106,58 @@ def attack_sample(feature_extractor, command_path, song_path, integrated_command
         logging.error("Only support Adam and SGD!")
         return
     
+    loss_feature_trend = torch.zeros(num_iterations)
+    loss_delta_trend = torch.zeros(num_iterations)
+    loss_trend = torch.zeros(num_iterations)
+    
     # Minimize the objective: ||integrated_command_feature - command_feature|| + penalty * ||delta||
     for i in range(num_iterations):
-        optimizer.zero_grad()
         integrated_command_feature = feature_extractor(waveform=song+delta)
-        loss = torch.norm(integrated_command_feature - command_feature) + penalty * torch.norm(delta)
+
+        loss_feature = torch.norm(integrated_command_feature - command_feature)
+        loss_delta = torch.norm(delta)
+        loss = loss_feature + penalty * loss_delta
+        
+        loss_feature_trend[i] = loss_feature
+        loss_delta_trend[i] = loss_delta
+        loss_trend[i] = loss
+
         loss.backward()
         optimizer.step()
-    
-    logging.info("loss_feature = {:.4f}, loss_delta = {:.4f}, loss = {:.4f}".format(torch.norm(integrated_command_feature - command_feature).data, 
-                                                                                    torch.norm(delta).data, 
-                                                                                    torch.norm(integrated_command_feature - command_feature).data + penalty * torch.norm(delta).data))
-    logging.info("")
+        optimizer.zero_grad()
 
     integrated_command = song + delta
     integrated_command = integrated_command.clamp(min=-1.0, max=1.0).detach().cpu()
     song_clip = song.detach().cpu()
 
+    snr = get_snr(audio=song_clip, audio_with_noise=integrated_command)
+
     save(filepath=integrated_command_path, src=integrated_command, sample_rate=16000)
     save(filepath=song_clip_path, src=song_clip, sample_rate=16000)
+
+    logging.info("loss_feature = {:.4f}, loss_delta = {:.4f}, loss = {:.4f}, snr = {:.2f}".format(loss_feature.data, loss_delta.data, loss.data, snr))
+    logging.info("")
+
+    fig, ax = plt.subplots(nrows=1, ncols=3, sharex='row')
+    ax[0].plot(loss_feature_trend.detach().cpu())
+    ax[1].plot(loss_delta_trend.detach().cpu())
+    ax[2].plot(loss_trend.detach().cpu())
+    fig.savefig("{}.png".format(integrated_command_path))
+
+    return loss_feature, loss_delta, loss, snr
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument("-f", "--feature", type=int, default=3, help="")
+    parser.add_argument("-f", "--feature", type=int, default=1, help="")
     parser.add_argument("-c", "--command_csv", type=str, default="commands/commands.csv", help="")
     parser.add_argument("-s", "--song_dir", type=str, default="songs/", help="")
     parser.add_argument("-d", "--dir", type=str, default="integrated-commands/", help="")
     parser.add_argument("-i", "--interval", type=float, default=1, help="Time interval to intercept a song.")
-    parser.add_argument("-p", "--penalty", type=float, default=75, help="Weight of the norm of the malicious perturbation.")
+    parser.add_argument("-p", "--penalty", type=float, default=500, help="Weight of the norm of the malicious perturbation.")
     parser.add_argument("-o", "--optimizer", type=str, default="Adam", help="Integrated optimizers in PyTorch, including Adam and SGD.")
     parser.add_argument("-l", "--learning_rate", type=float, default=0.001, help="Learning rate used in the specified optimizer.")
-    parser.add_argument("-n", "--num_iterations", type=int, default=10000, help="The maximum number of iterations for the specified optimizer.")
+    parser.add_argument("-n", "--num_iterations", type=int, default=3000, help="The maximum number of iterations for the specified optimizer.")
     parser.add_argument("-g", "--gpu", type=str, default='0', help="GPU index to use.")
 
     args = parser.parse_args()
