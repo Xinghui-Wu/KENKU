c0dee95cc02ffd6d384b8fcef4af5721f79f14c7
wuxinghui <xjtu.wuxinghui@gmail.com>
2021-11-29 20:27:15 +0800

    Calculate SNR metric in utils.py for the attacks.

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import csv
 
+import numpy as np
+
 
 def read_csv(csv_path):
     with open(csv_path, 'r') as csv_file:
@@ -20,7 +22,7 @@ def get_feature_parameters(feature):
 
     n_mfcc_list = (12, 13, 20, 26, 40)
     n_mels_list = (64, 128, 256, 512)
-    n_fft_list = (512, 1024, 2048)
+    n_fft_list = (400, 512, 1024, 2048)
 
     if feature == 1:
         for n_fft in n_fft_list:
@@ -36,3 +38,18 @@ def get_feature_parameters(feature):
                     feature_parameters.append({"n_mfcc": n_mfcc, "n_mels": n_mels, "n_fft": n_fft})
     
     return feature_parameters
+
+
+def get_snr(audio, audio_with_noise):
+    length = min(len(audio), len(audio_with_noise))
+    
+    audio = audio[: length]
+    audio_with_noise = audio_with_noise[: length]
+    noise = audio_with_noise - audio
+
+    p_audio = np.sum(audio ** 2)
+    p_noise = np.sum(noise ** 2)
+
+    snr = 10 * np.log10(p_audio / p_noise)
+
+    return snr
